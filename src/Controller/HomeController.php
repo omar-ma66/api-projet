071443b8ff1e1a87205425f575c1861da2a7983e
omar-ma66 <devcodefree66@gmail.com>
2026-07-16 16:49:13 +0200

    je dois tester la route /api/admin elle pose un probleme

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,6 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'api_home')]
-
     public function hello()
     {
       
@@ -19,5 +18,16 @@ final class HomeController extends AbstractController
         // ]);
 
         return $this->render('home/home.html.twig');
+    }
+
+      #[Route('/admin', name: 'api_admin')]
+    public function office()
+    {
+      
+        // return $this->json([
+        //     'message' => "Bienvenue sur l'API de BookMarket ! 🎉",
+        // ]);
+
+        return $this->json(["message"=>"super ca marche"],status:200);
     }
 }
