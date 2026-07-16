bf5286df6a00295ce340168ad05c2b02ff09b88e
omar-ma66 <devcodefree66@gmail.com>
2026-07-16 21:41:44 +0200

    mise en service sur mon pc

--- migrations/Version20260716173446.php
+++ migrations/Version20260716173446.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260715141431 extends AbstractMigration
+final class Version20260716173446 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,6 @@ final class Version20260715141431 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('ALTER TABLE category_book ADD CONSTRAINT FK_407ED97612469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE category_book ADD CONSTRAINT FK_407ED97616A2B381 FOREIGN KEY (book_id) REFERENCES book (id) ON DELETE CASCADE');
     }
@@ -28,7 +27,6 @@ final class Version20260715141431 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE user');
         $this->addSql('ALTER TABLE category_book DROP FOREIGN KEY FK_407ED97612469DE2');
         $this->addSql('ALTER TABLE category_book DROP FOREIGN KEY FK_407ED97616A2B381');
     }
