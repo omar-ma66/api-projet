1280ece89f5ec1c66b49eac0796e51aacfed8611
omar-ma66 <devcodefree66@gmail.com>
2026-07-17 16:56:09 +0200

    realise le BookDataPersister

--- src/DataPersister/UserDataPersister.php
+++ src/DataPersister/UserDataPersister.php
@@ -1,36 +1,5 @@
 <?php
-// Ce fichier est a créer dans src/DataPersister
-// namespace App\DataPersister;
 
-// use ApiPlatform\Metadata\Operation;
-// use ApiPlatform\State\ProcessorInterface;
-// use App\Entity\User;
-// use Doctrine\ORM\EntityManagerInterface;
-// use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-
-// class UserDataPersister implements ProcessorInterface
-// {
-//     public function __construct(
-//         private readonly EntityManagerInterface $entityManager,
-//         private readonly UserPasswordHasherInterface $passwordHasher
-//     ) {}
-
-//     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): User
-//     {
-//         if ($data instanceof User) {
-//             if ($data->getPassword()) {
-//                 $hashedPassword = $this->passwordHasher->hashPassword($data, $data->getPassword());
-//                 $data->setPassword($hashedPassword);
-//             }
-//             $data->setRoles(['ROLE_USER']);
-
-//             $this->entityManager->persist($data);
-//             $this->entityManager->flush();
-//         }
-
-//         return $data;
-//     }
-// }
 
 namespace App\DataPersister;
 
